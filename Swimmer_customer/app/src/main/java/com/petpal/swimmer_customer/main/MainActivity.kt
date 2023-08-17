cd649654a9ee0ccae2eb3708b7b41064d396a9c5
gayeon00 <chlrkdus4819@gmail.com>
2023-08-17 11:45:26 +0900

    init : 프로젝트 초기 설정

---
+++ Swimmer_customer/app/src/main/java/com/petpal/swimmer_customer/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.petpal.swimmer_customer.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.petpal.swimmer_customer.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ Swimmer_customer/app/src/main/java/com/petpal/swimmer_customer/main/MainFragment.kt
@@ -0,0 +1,30 @@
+package com.petpal.swimmer_customer.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.petpal.swimmer_customer.R
+import com.petpal.swimmer_customer.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    lateinit var fragmentMainBinding: FragmentMainBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentMainBinding = FragmentMainBinding.inflate(layoutInflater)
+
+        val navHostFragment =
+            childFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        fragmentMainBinding.bottomNavigation.setupWithNavController(navController)
+
+        return fragmentMainBinding.root
+    }
+
+}
