8e9389858753c0c6dbaecded1a507fe4b3822d0c
gayeon00 <chlrkdus4819@gmail.com>
2023-08-17 22:28:04 +0900

    refactor: [판매자용] 로그인 및 회원가입 LogginUser 삭제
    - Firebase.auth.currentUser로 캐싱가능하기 때문에 LoggedInUser 클래스 삭제

--- Swimmer_seller/app/src/main/java/com/petpal/swimmer_seller/data/UserRepository.kt
+++ Swimmer_seller/app/src/main/java/com/petpal/swimmer_seller/data/UserRepository.kt
@@ -2,12 +2,8 @@ package com.petpal.swimmer_seller.data
 
 import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.AuthResult
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import com.petpal.swimmer_seller.data.model.LoggedInUser
-import kotlinx.coroutines.tasks.await
-import java.io.IOException
 
 /**
  * Class that requests authentication and user information from the remote data source and
@@ -15,36 +11,20 @@ import java.io.IOException
  */
 
 class UserRepository {
-
-    // in-memory cache of the loggedInUser object
-    //TODO : firebase로 변경할 수 없나?
-    var user: LoggedInUser? = null
-        private set
-
-    val isLoggedIn: Boolean
-        get() = user != null
-
     val auth = Firebase.auth
 
-    init {
-        // If user credentials will be cached in local storage, it is recommended it be encrypted
-        // @see https://developer.android.com/training/articles/keystore
-        user = null
-    }
-
     fun logout() {
-        user = null
         auth.signOut()
     }
 
-    fun login(email: String, password: String, callback: (Task<AuthResult>) -> Unit){
+    fun login(email: String, password: String, callback: (Task<AuthResult>) -> Unit) {
         auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(callback)
         //user를 살리려면 여기서 집어넣어줘야함
     }
 
     fun signUp(email: String, password: String, callback: (Task<AuthResult>) -> Unit) {
         //로그인된 유저가 없을 때만
-        if (user == null) {
+        if (auth.currentUser == null) {
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(callback)
             //user를 살리려면 여기서도 집어넣어줘야함
--- Swimmer_seller/app/src/main/java/com/petpal/swimmer_seller/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.petpal.swimmer_seller.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from UserRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
