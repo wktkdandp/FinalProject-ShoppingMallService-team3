0dfe39e697f4ee3864075d4431a0f80f615f3a41
gayeon00 <chlrkdus4819@gmail.com>
2023-08-30 09:51:29 +0900

    refactor: 주문완료 페이지에 주문내역으로 가기 활성화 시키기

--- Swimmer_customer/app/src/main/java/com/petpal/swimmer_customer/ui/payment/CompleteFragment.kt
+++ Swimmer_customer/app/src/main/java/com/petpal/swimmer_customer/ui/payment/CompleteFragment.kt
@@ -2,11 +2,11 @@ package com.petpal.swimmer_customer.ui.payment
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.petpal.swimmer_customer.R
@@ -20,7 +20,7 @@ class CompleteFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         fragmentCompleteBinding = FragmentCompleteBinding.inflate(layoutInflater)
         mainActivity = activity as MainActivity
@@ -30,7 +30,7 @@ class CompleteFragment : Fragment() {
         fragmentCompleteBinding.run {
 
             // countdownTimer 로 사용자가 클릭을 하지 않아도 자동 화면 전환
-            val countDownTimer = object: CountDownTimer(4000, 1000) {
+            val countDownTimer = object : CountDownTimer(4000, 1000) {
 
                 override fun onTick(p0: Long) {
                     completeTextCountdown.text = "${(p0 / 1000)}초 뒤 홈 화면으로 이동합니다."
@@ -40,7 +40,7 @@ class CompleteFragment : Fragment() {
                 override fun onFinish() {
 
                     // homeFragment로 이동
-                    val action=CompleteFragmentDirections.actionCompleteFragmentToItemHome()
+                    val action = CompleteFragmentDirections.actionCompleteFragmentToItemHome()
                     findNavController().navigate(action)
                 }
 
@@ -53,12 +53,15 @@ class CompleteFragment : Fragment() {
                     .navigate(R.id.item_home)
             }
             completeDetail.setOnClickListener {
-
+                countDownTimer.cancel()
+                //주문목록으로 이동
+                findNavController().navigate(R.id.action_completeFragment_to_orderListFragment)
             }
         }
 
         return fragmentCompleteBinding.root
     }
+
     private fun handleBackPress() {
         val callback = object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
